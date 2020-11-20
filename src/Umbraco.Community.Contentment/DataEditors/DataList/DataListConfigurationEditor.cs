761a7e7e0c8c28ad2ebe4c36342802f956eea3c5
leekelleher <leekelleher@gmail.com>
2020-11-20 17:32:01 +0000

    [WIP] Preview for Data List editor
    When configuring the data-type, a preview panel will appear
    at the bottom, displaying the rendered list-editor and a table
    of the data-source items.

--- src/Umbraco.Community.Contentment/DataEditors/DataList/DataListConfigurationEditor.cs
+++ src/Umbraco.Community.Contentment/DataEditors/DataList/DataListConfigurationEditor.cs
@@ -60,6 +60,12 @@ namespace Umbraco.Community.Contentment.DataEditors
                     { "addButtonLabelKey", "contentment_configureListEditor" },
                     { Constants.Conventions.ConfigurationFieldAliases.Items, listEditors }
                 });
+
+            Fields.Add(
+                "preview",
+                "Preview",
+                null,
+                IOHelper.ResolveUrl(DataListDataEditor.DataEditorPreviewViewPath));
         }
 
         public override IDictionary<string, object> ToValueEditor(object configuration)
